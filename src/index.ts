457ffea83ec7bcd9ee1706a5ecf225e8eebbe643
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-04 19:59:18 -0300

    feat: puppeteer working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('oi')
+import puppeteer from 'puppeteer'
+
+async function main() {
+
+
+    const browser = await puppeteer.launch()
+
+    const page = await browser.newPage()
+    await page.goto('https://www.google.com/')
+
+    const html = await page.evaluate(() => document.querySelector('html').innerHTML)
+
+    await browser.close()
+    
+    return html
+
+}
+
+main()
+    .then(console.log)
